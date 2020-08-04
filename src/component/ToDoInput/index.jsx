87fd0ea008fda274bb9a533a616bd0f4ba674cd1
jenkincstang <jenkincs@163.com>
2020-08-04 14:33:12 +0800

    [todolist][jenkin] feature: add ToDoInput Component

---
+++ src/component/ToDoInput/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+
+class ToDoInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (<div>
+        <input></input><button>ADD</button>
+        </div>)
+    }
+}
+export default ToDoInput
